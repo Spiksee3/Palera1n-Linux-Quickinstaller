db1bc9c7d1fcd803c8b706adba994bb9809632be
Spiksee3 <81059802+Spiksee3@users.noreply.github.com>
2023-01-03 06:09:23 +0000

    Add files via upload

---
+++ Palera1n.sh
@@ -0,0 +1,23 @@
+cd ..
+
+cd desktop
+
+﻿sudo apt-get update
+
+sudo add-apt-repository universe
+
+sudo apt-get update
+
+sudo apt install libimobiledevice-utils libusbmuxd-tools git curl python3-pip -y
+
+sudo pip install pyimg4
+
+wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.16_amd64.deb
+
+sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2.16_amd64.deb
+
+sudo apt-get update
+
+sudo git clone --recursive https://github.com/palera1n/palera1n && cd palera1n
+
+firefox https://ictfix.net/gaming/how-to-install-palera1n-jailbreaki-ios-15-16-on-windows/
